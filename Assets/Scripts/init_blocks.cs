19374263f53d8a7f8e4988d370e9787b17c9bba5
Tyco <54979017+Tycovds@users.noreply.github.com>
2021-01-05 13:31:45 +0100

    Added folder

---
+++ Assets/Scripts/init_blocks.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class init_blocks : MonoBehaviour
+{
+    //Get cube prefab
+    public GameObject cube;
+    public Slider size_slider;
+    public Slider column_slider;
+    public Slider row_slider;
+    public Button reset;
+    int size = 2;
+    int columns = 3;
+    int rows = 3;
+  
+    void Start()
+    {
+
+        init_cubes();
+        
+        //listen for reset button
+        reset.onClick.AddListener(resetScene);
+    }
+    //reset scene with new slider values
+    void resetScene()
+    {
+        //get current cubes in scene
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("cube");
+        //get every cube in list and destroy it
+        foreach (GameObject cube in cubes)
+        {
+            Destroy(cube);
+        }
+        //get slider values
+        size = (int)size_slider.value;
+        columns = (int)column_slider.value;
+        rows = (int)row_slider.value;
+        //make new cubes
+        init_cubes();
+    }
+
+    //make cubes based on size, columns and rows
+    void init_cubes()
+    {
+        for (int i = 0; i < columns; ++i)
+        {
+            for (int j = 0; j < rows; ++j)
+            {
+                //Change the cube prefab scale with the size we want
+                cube.transform.localScale = new Vector3(size, size, size);
+
+                //Instantiate a new cube in the middle of the camera with the right column (x) and row (y) coördinates
+                Instantiate(cube, new Vector3(i * size - Mathf.Ceil(columns * size / 2), j * size + Mathf.Ceil(rows * size / 2), 2), Quaternion.identity);
+            }
+        }
+    }
+   
+}
---
+++ Assets/Scripts/mouse_down.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class mouse_down : MonoBehaviour
+{
+    public GameObject cube;
+    public Rigidbody body;
+
+    //When the mouse hovers over the cubes, it turns to this color
+    Color mouseover_color = new Color32(255, 68, 68, 255);
+
+    //This stores the cubes original color
+    Color original_color;
+
+    //Get the cubes mesh renderer to access the color
+    MeshRenderer mesh_renderer;
+
+    public int torque = 20;
+
+    void Start()
+    {
+        //Get the mesh renderer component from the cube
+        mesh_renderer = GetComponent<MeshRenderer>();
+
+        //Fetch the original color of the GameObject
+        original_color = mesh_renderer.material.color;
+    }
+
+    void OnMouseOver()
+    {
+        // Change the color of the GameObject to red when the mouse is over GameObject
+        mesh_renderer.material.color = mouseover_color;
+
+        //Rotate the cube with w a s d and jump space
+        if (Input.GetKey("w"))
+        {
+            body.AddTorque(torque, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            body.AddTorque(-torque, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            body.AddTorque(0, 0, torque);
+        }
+        if (Input.GetKey("d"))
+        {
+            body.AddTorque(0, 0, -torque);
+        }
+        if (Input.GetKey("space"))
+        {
+            body.AddForce(0, 10, 0);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        // Reset the color of the cube back to normal
+        mesh_renderer.material.color = original_color;
+    }
+
+}
